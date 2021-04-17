ab822105b18c9061b6b3bf52bfc4c1ea27a29ccc
esteemeran <76672853+esteemeran@users.noreply.github.com>
2021-04-17 23:42:23 +1000

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -3,19 +3,39 @@ var health = 100;
 var coins = 100;
 var currentState = 0;
 
-var button = '<input type="button" name="Action1" value="Action1" onclick="nextPage(this.form)" />';
+var FromDB_task = 
+{
+    descr: "Ваша карта заблокирована из-за подозрительной активности. Чтобы снять блокировку, в течении 500 секунд вам необходимо пройти по ссылке https://***.***. Ваш ЦентроБанк",
+    question: true,
+answers: [
+    {
+        descr: "Перейти по ссылке из сообщения",
+value: false},
+{
+    descr: "Проверить состояния счетов через официальный сайт банка",
+    value: true
+    }
+]
+}
 
-var nextPage = function (form) {
+var buttonStart = '<input type="button" name="Action1" value="';
+var buttonMid = '" onclick="nextPage(this.form';
+var buttonEnd = ')" />';
+
+var textStart = '<div name="discr">';
+var textEnd = '</div>';
+
+var nextPage = function (form, answer) {
     var result = "";
     if (currentState == 0)
     {
         currentState++;
-        result += button;
-        result += button;
-
-
-
+        result += textStart + FromDB_task.descr + textEnd;
+        for (var i = 0; i<FromDB_task.answers.length;i++)
+        {
+            var temp = FromDB_task.answers[i];
+            result += buttonStart + temp.descr + buttonMid + ", " + temp.value + buttonEnd;
+       }
+        form.innerHTML = result;
     }
-    form.innerHTML = result;
 }
-
